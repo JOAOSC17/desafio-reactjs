9823c799b0173c2b990c9351bc684332710c5734
João Costa <costav1402@gmail.com>
2021-07-22 10:50:16 -0300

    estilizando o repositorio

--- desafio-reactjs/src/Components/Profile/Repository/Repository.js
+++ desafio-reactjs/src/Components/Profile/Repository/Repository.js
@@ -1,12 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import api from 'Services/api';
 import './Repository.css'
+import { BsFillCircleFill, BsStar } from 'react-icons/bs'
+import { Link } from 'react-router-dom';
+function showDate(dateString, daysMonthsYears){
+    const dateToShow = new Date(dateString);
+    if(!dateString || !dateToShow.getTime()) return '';
+    const { days, months, years} = daysMonthsYears;
+    if (days) dateToShow.setDate(dateToShow.getDate() + days);
+    if (months) dateToShow.setMonth(dateToShow.getMonth() + months);
+    if (years) dateToShow.setFullYear(dateToShow.getFullYear() + years);
+    return dateToShow.toLocaleDateString();
+}
 const ProfilesRepository = ({login}) => {
     const [repository, repositoryInfo]= useState();
-
+    const [ date, setDate ] = useState('');
+    const [ daysMonthsYears, setDaysMonthsYears ] = useState({
+        days:0,
+        month:0,
+        years:0,
+    });
     useEffect(()=>{
-      
-          console.log(login)
+            console.log(login)
               api
              .get(`/${login}/repos`)
              .then((response) => {
@@ -16,21 +31,30 @@ const ProfilesRepository = ({login}) => {
              .catch((err) => {
                console.error("ops! ocorreu um erro" + err);
              })
+
           // eslint-disable-next-line react-hooks/exhaustive-deps
       },[])
+        
+
+      
       if (!repository){
-          return <div>Carregando...</div>
-      }
+        return <div>Loading...</div>
+    }
     return (
-          <div> 
-         <h4>{repository[0].name}</h4>
+          <div className="profiles-repository"> 
+          <a className="profiles-repository__link" href={`https://github.com/${repository[0].full_name}`} rel="noreferrer"  target='_blank'>
+         <h4 className="profiles-repository__name">{repository[0].name}</h4>
                          
-      <p>{(repository[0].description!==null)?(`${repository.description}`) :('No description, website, or topics provided')}</p>
-                <div>
-                 <span>stars</span>
-                 <span>Updated n days ago</span>
+      <p className="profiles-repository__description">{(repository[0].description!==null)?(`${repository.description}`) :('No description, website, or topics provided')}loremLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet, augue et rutrum varius, orci nunc mollis leo, quis mollis risus erat in leo. Vivamus nec mauris quis ipsum mollis sollicitudin. Nullam et commodo elit. Mauris porta blandit urna, mattis blandit ante elementum sit amet. Quisque malesuada, enim molestie blandit vulputate, purus augue luctus turpis, vitae lacinia risus metus at nisl. Fusce aliquam sodales est a placerat. Fusce ultrices, elit vel ornare posuere, orci lorem tempus neque, et porta justo neque eget lorem. Mauris molestie feugiat pharetra.
+
+Suspendisse sit amet magna mi. Etiam interdum massa leo, non iaculis ex sagittis non. Aenean facilisis lorem in ullamcorper consequat. Donec quis nisl ut magna hendrerit feugiat ac tincidunt ipsum. Nunc nec nunc eget odio lacinia cursus. Vestibulum eu luctus enim. Maecenas id ultricies libero. Nullam vel turpis suscipit, gravida elit quis, consequat quam. Nulla mattis enim eu consectetur volutpat. Phasellus pretium, urna eget maximus condimentum, erat eros egestas nibh, a sodales mi ex vitae leo. Curabitur nisi neque, tristique non est sit amet, pellentesque tristique neque.
+
+Donec a pulvinar est. Vivamus et venenatis felis, sed sodales enim. Nulla hendrerit rutrum diam, </p>
+                <div className="profiles-repository-info">
+                 <span><BsStar/> {`${repository[0].star} stars`}</span>
+                 <span><BsFillCircleFill/> Updated n days ago</span>
                  </div>
-                 
+     </a>
      </div>
     )
 }
